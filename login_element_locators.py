1243c82c51872cbc246ab8b3ed347762b0f09cbb
Vallarasu Sambathkumar <vals@live.in>
2026-04-19 19:08:35 +0530

    Update login_element_locators.py

--- login_element_locators.py
+++ login_element_locators.py
@@ -5,12 +5,39 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
+# Browser / Driver
+    # - Create an instance / object or drive
+    # interacting with it
+        # - get
+        # - close
+        # - quit
+        # - refresh
+        # - maximize window
+
+# Page
+    # - title
+    # - current_url 
+
+# Elements
+    # find_element
+    # find_elements
+        # click
+        # clear
+        # send_keys
+
 def test_login_with_valid_credentials():
     # Arrange
     driver = webdriver.Firefox()
     driver.implicitly_wait(5)
 
     driver.get("https://cs101.in/ecom/login.html")
+
+    # //*[@id="auth-button"]
+    # //*[@id="auth-button"]
+
+    # //button[contains(text(), 'Next')]
+    # //button[contains(@class, 'signup')]
+    # //button[contains(@name, 'signup')]
 
     user_name_locator = "//input[@id='auth-user']"
     password_locator = "//input[@name='auth-pass']"
