9586968c139ae39d58ab74d433e56e227b47ceba
Vallarasu Sambathkumar <vals@live.in>
2026-05-30 19:54:23 +0530

    Update test_login_pytest.py

--- test_login_pytest.py
+++ test_login_pytest.py
@@ -6,14 +6,24 @@ import pytest
 
 
 @pytest.fixture
-def credential():
-    file = open(r"C:\Users\Valla\Documents\GitHub\py-selenium\credential.csv", "r")
-    data = file.readline()
-    file.close()
-    [user_name, password] = data.split(",")
-    return (user_name, password)
+def category():
+    return "electronics"
 
-# json
+@pytest.fixture
+def product(category):
+    return "laptop", category
+
+
+@pytest.fixture
+def credential():
+    # file = open(r"C:\Users\Valla\Documents\GitHub\py-selenium\credential.csv", "r")
+    # data = file.readline()
+    # file.close()
+    # [user_name, password] = data.split(",")
+    # return (user_name, password)
+
+    return "admin", "p@ss1234"
+
 
 def test_login_works(credential):
 
@@ -30,7 +40,71 @@ def test_login_works(credential):
     password_el = driver.find_element(By.XPATH, password_locator)
     login_button = driver.find_element(By.ID, login_locator)
 
-    # credential = get_credentials()
+    # credential = credential()
+    user_name, password = credential
+    prod, cat = product
+
+    # Act
+    user_el.send_keys(user_name)
+    password_el.send_keys(password)
+
+    time.sleep(5)
+
+    login_button.click()
+
+    # Assert
+    assert driver.current_url != "Login Auth Automation Testing"
+    driver.quit()
+
+
+def test_order_works(credential, product):
+
+    #  Arrange
+    url = "https://vallarasu.in/test/login"
+    user_locator = "//input[@id='login-user']"
+    password_locator = "//input[@data-testid='login-pass']"
+    login_locator = "login-button"
+
+    driver = webdriver.Firefox()
+    driver.get(url)
+
+    user_el = driver.find_element(By.XPATH, user_locator)
+    password_el = driver.find_element(By.XPATH, password_locator)
+    login_button = driver.find_element(By.ID, login_locator)
+
+    # credential = credential()
+    user_name, password = credential
+
+    # Act
+    user_el.send_keys(user_name)
+    password_el.send_keys(password)
+
+    time.sleep(5)
+
+    login_button.click()
+
+    # Assert
+    assert driver.current_url != "Login Auth Automation Testing"
+    driver.quit()
+
+
+
+@pytest.mark.parametrize("credential", [("admin", "admin"), ("admin", "pass"), ("admin", "1234") ])
+def test_multiple_login(credential):
+    #  Arrange
+    url = "https://vallarasu.in/test/login"
+    user_locator = "//input[@id='login-user']"
+    password_locator = "//input[@data-testid='login-pass']"
+    login_locator = "login-button"
+
+    driver = webdriver.Firefox()
+    driver.get(url)
+
+    user_el = driver.find_element(By.XPATH, user_locator)
+    password_el = driver.find_element(By.XPATH, password_locator)
+    login_button = driver.find_element(By.ID, login_locator)
+
+    # credential = credential()
     user_name, password = credential
 
     # Act
@@ -47,3 +121,49 @@ def test_login_works(credential):
 
 if __name__ == "__main__":
     test_login_works()
+
+
+# Py Test Scope
+
+def add(x, y):
+    return x + y
+
+@pytest.fixture()
+def values():
+    # reading from file
+    return [(1,2, 3), (-1,2,1), (-1,-2, -3)]
+
+@pytest.fixture(scope="class")
+def driver():
+    firefox = webdriver.Firefox()
+    return firefox
+
+class TestAdd:
+
+    def test_add_values(values):
+
+        for params in values:
+            validate_add(params)
+        values.clear()
+
+
+    def test_add_values_two (values):
+        for params in values:
+            validate_add(params)
+
+
+    def validate_add(params):
+
+        # Arrange
+        x, y, expected = params
+
+        # Act
+        result = add(1, 2)
+
+        # Assert
+        assert result == 3
+
+# Py Test Fixture Scope
+    # function
+    # class
+    # 
